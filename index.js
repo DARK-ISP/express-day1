35a1ed1a80ba850ad4597ce532022ca71dfd4d4d
Your Name <you@example.com>
2023-11-06 21:42:34 +0545

    express day 1 update

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("express day 1")
+import express from "express";
+
+const app = express();
+
+app.get("/",(req,res)=>{
+    res.send("welcome to js");
+})
+
+app.get("/node",(req,res)=>{
+    res.send("thanks for comming")
+})
+
+const port = 8080;
+
+app.listen(port,(req,res)=>{
+    console.log("hello")
+    console.log(`server is running on ${port}`);
+})
+
