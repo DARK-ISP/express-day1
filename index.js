e8633dab12d7d2c894504aabcd000e42cbe28a3a
Your Name <you@example.com>
2023-11-11 11:08:56 +0545

    view in node js use .get method with postman

--- index.js
+++ index.js
@@ -2,18 +2,52 @@ import express from "express";
 
 const app = express();
 
-app.get("/",(req,res)=>{
-    res.send("welcome to js");
+app.use(express.json());
+
+
+
+//customer list
+
+
+const customer = [
+    {
+        id: 1,
+        name: "dark isp",
+        address: "github"
+    },
+    {
+        id: 2,
+        name: "code",
+        address: "git"
+    }
+];
+
+
+//view customer details
+
+app.get("/details",(req,res)=>{
+     return res.status(200).send(customer);
 })
 
-app.get("/node",(req,res)=>{
-    res.send("thanks for comming")
+app.get("/",(req,res)=>{
+   
+   const name = [];
+    for(const customers of customer){
+        name.push(`<br> <br>${customers.name}`)
+    }
+    return res.send(`name list of all customer : <br></br> ${name}`);
 })
+
+
+
+
+
+
+
+
 
 const port = 8080;
 
-app.listen(port,(req,res)=>{
-    console.log("hello")
-    console.log(`server is running on ${port}`);
+app.listen(port,()=>{
+    console.log(`server is running on port ${port}`);
 })
-
